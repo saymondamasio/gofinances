339388026e7be4dadae0467eeafe9df120a52ec8
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-01 20:18:24 -0300

    feat: config theme with styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { Container } from './styles'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  )
+}
