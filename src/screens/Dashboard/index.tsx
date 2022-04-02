60582fc5e0ae4f06683dff188b2d9a579cc21eb3
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-02 12:07:20 -0300

    fdeat: add highlight card component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,55 @@
-import { Container, Title } from './styles'
+import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
+import {
+  Container,
+  Header,
+  HighlightCards,
+  Icon,
+  Photo,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserInfoWrapper,
+  UserName,
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserInfoWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://github.com/saymondamasio.png' }} />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Saymon</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserInfoWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.400,00"
+          lastTransaction="Última saída dia 04 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 5.400,00"
+          lastTransaction="01 á 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,73 @@
+import { Feather } from '@expo/vector-icons'
+import Constants from 'expo-constants'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.background};
 `
 
-export const Title = styled.Text`
-  font-size: 24px;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+`
+export const UserInfoWrapper = styled.View`
+  width: 100%;
+
+  padding: 0 24px;
+  margin-top: ${RFValue(Constants.statusBarHeight)}px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+
+  border-radius: 10px;
+`
+
+export const User = styled.View`
+  margin-left: 17px;
+`
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(18)}px;
+`
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+` as unknown as typeof Feather
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `
