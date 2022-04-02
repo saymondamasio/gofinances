72019f8b2d19486e4d4298c8e483ee1e7662d4cb
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-02 13:58:36 -0300

    feat: add component in Register screen

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import AppLoading from 'expo-app-loading'
 import React from 'react'
 import { ThemeProvider } from 'styled-components/native'
 import theme from './src/global/styles/theme'
-import { Dashboard } from './src/screens/Dashboard'
+import { Register } from './src/screens/Register'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
