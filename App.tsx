7f1a116b971214162c0d60e333eab27e112bd451
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-03 15:18:30 -0300

    feat: create tab navigation

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import {
   Poppins_700Bold,
   useFonts,
 } from '@expo-google-fonts/poppins'
+import { NavigationContainer } from '@react-navigation/native'
 import AppLoading from 'expo-app-loading'
 import React from 'react'
 import { ThemeProvider } from 'styled-components/native'
 import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register'
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +23,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
