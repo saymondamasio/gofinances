d201d52660ff2f258ab0560c1db622489fc8b345
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-03 22:49:08 -0300

    feat: add HistoryCard component

---
+++ src/components/HistoryCard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Amount, Container, Title } from './styles'
+
+interface Props {
+  title: string
+  color: string
+  amount: string
+}
+
+export function HistoryCard({ amount, color, title }: Props) {
+  return (
+    <Container color={color}>
+      <Title>{title}</Title>
+
+      <Amount>{amount}</Amount>
+    </Container>
+  )
+}
