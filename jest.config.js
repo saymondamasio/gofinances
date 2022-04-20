d049d59ea822e466f74dcfdf7dd9e503c203f9c8
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-20 09:39:38 -0300

    chore: add @testing-library/react-hooks

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/android', '/ios'],
 }
